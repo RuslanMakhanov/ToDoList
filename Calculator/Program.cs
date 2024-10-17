714e7e4357d1aaafaf6a231908557ab48138fe2e
RuslanNnNnNn <95371964+RuslanMakhanov@users.noreply.github.com>
2024-10-17 15:07:53 +0500

    Created Calculator Project

---
+++ Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Hello! \nInput the first number: ");
+string userinput = Console.ReadLine();
+int num1 = int.Parse(userinput);
+
+Console.WriteLine("Input the second number: ");
+userinput = Console.ReadLine();
+int num2 = int.Parse(userinput);
+
+Console.WriteLine("What do you want to do with those numbers?\r\n[A]dd\r\n[S]ubtract\r\n[M]ultiply\r\n");
+userinput = Console.ReadLine();
+if (EqualCaseInsensitive(userinput,"A"))
+{
+    int result = num1 + num2;
+    PrintFinalEquation(num1, num2, result, "+");
+}
+else if (EqualCaseInsensitive(userinput, "S"))
+{
+    int result = num1 - num2;
+    PrintFinalEquation(num1, num2, result, "-");
+}
+else if (EqualCaseInsensitive(userinput, "M"))
+{
+    int result = num1 * num2;
+    PrintFinalEquation(num1, num2, result, "*");
+}
+else
+{
+    Console.WriteLine("Invalid option");
+}
+
+void PrintFinalEquation(int number1, int number2, int equals, string @operator  )
+{
+    Console.WriteLine(number1 + " " + @operator + " " + number2 + " = " + equals);
+}
+
+bool EqualCaseInsensitive(string left, string right)
+{
+    return left.ToUpper().Equals(right.ToUpper());
+}
+
+Console.ReadKey();
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,2 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("What Do You Want?");
+Console.WriteLine("[S]ee all TODOS");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = Console.ReadLine();
+
+if (userInput == "S")
+{
+    PrintSelectedOption("See all TODOS");
+}
+else if (userInput == "A")
+{
+    PrintSelectedOption("Add a TODO");
+}
+else if (userInput == "R")
+{
+    PrintSelectedOption("Remove a TODO");
+}
+else if (userInput == "E")
+{
+    PrintSelectedOption("Exit");
+}
+else
+{
+    PrintSelectedOption("Is not valid");
+}
+
 Console.ReadKey();
+
+void PrintSelectedOption(string selectedOption)
+{
+    Console.WriteLine("Selected Option: " + selectedOption);
+}
